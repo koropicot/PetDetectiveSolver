17973a49eff545201fec4ae7c9f3890269793611
koropicot <koropicot@gmail.com>
2015-02-10 22:14:23 +0900

    PriorityQueueを作成

---
+++ PetDetectiveSolver/PriorityQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetDetectiveSolver
+{
+    public class PriorityQueue<T>
+    {
+        private List<T> array;
+        private IComparer<T> comparer 
+            = Comparer<T>.Create((x,y) => Comparer<T>.Default.Compare(y,x));
+
+        public int Count { get { return array.Count; } }
+
+        public PriorityQueue() : this(new List<T>(), Comparer<T>.Default) { }
+
+        public PriorityQueue(IComparer<T> comparer) : this(new List<T>(), comparer) { }
+
+        public PriorityQueue(IEnumerable<T> source) : this(source, Comparer<T>.Default) { }
+
+        public PriorityQueue(IEnumerable<T> source, IComparer<T> comparer) : this(source.ToList(), comparer) { }
+
+        private PriorityQueue(List<T> source, IComparer<T> comparer)
+        {
+            array = source;
+            this.comparer = comparer;
+            array.Sort(Comparer<T>.Create((x,y)=>comparer.Compare(y,x)));
+        }
+
+        public void Enqueue(T item)
+        {
+            array.Add(default(T));
+            var current = array.Count - 1;
+            while (current != 0)  //入れる場所まで辿る
+            {
+                var parent = (current - 1) / 2;
+                if (comparer.Compare(array[parent], item) >= 0)
+                    break;
+                array[current] = array[parent];
+                current = parent;
+            }
+            array[current] = item;
+        }
+
+        public T Dequeue()
+        {
+            if (array.Count == 0)
+                throw new InvalidOperationException("The queue is empty.");
+            var ret = array[0];
+            var last = array[array.Count - 1];
+            array.RemoveAt(array.Count - 1);
+            var lastIndex = array.Count - 1;
+
+            var current = 0;
+            while (current * 2 + 1 <= lastIndex)
+            {
+                var childL = current * 2 + 1;
+                var childR = childL + 1;
+                var child = (childR > lastIndex) || (comparer.Compare(array[childL], array[childR]) >= 0) ? childL : childR;
+                if (comparer.Compare(last, array[child]) >= 0)
+                    break;
+                array[current] = array[child];
+                current = child;
+            }
+            if(array.Count > 0)
+                array[current] = last;
+
+            return ret;
+        }
+    }
+}
---
+++ PetDetectiveSolverTest/PriorityQueueTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetDetectiveSolver;
+
+namespace PetDetectiveSolverTest
+{
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        [TestMethod]
+        public void エンキューした値は優先度順でデキューされる()
+        {
+            var priorityQueue = new PriorityQueue<int>(new[] { 3, 5, 6 });
+
+            priorityQueue.Enqueue(2);
+            priorityQueue.Enqueue(8);
+
+            Assert.AreEqual(8, priorityQueue.Dequeue());
+            Assert.AreEqual(6, priorityQueue.Dequeue());
+            Assert.AreEqual(5, priorityQueue.Dequeue());
+            Assert.AreEqual(3, priorityQueue.Dequeue());
+            Assert.AreEqual(2, priorityQueue.Dequeue());
+        }
+
+        [TestMethod]
+        public void 与えたComparerが優先度の比較に用いられる()
+        {
+            var priorityQueue = new PriorityQueue<Tuple<int, int>>(
+                Comparer<Tuple<int, int>>.Create((t1, t2) =>
+                    t1.Item1 == t2.Item1
+                        ? t1.Item2 - t2.Item2
+                        : t1.Item1 - t2.Item1));
+
+            priorityQueue.Enqueue(Tuple.Create(1, 1));
+            priorityQueue.Enqueue(Tuple.Create(2, 1));
+            priorityQueue.Enqueue(Tuple.Create(1, 3));
+            priorityQueue.Enqueue(Tuple.Create(2, 2));
+            priorityQueue.Enqueue(Tuple.Create(3, 2));
+
+
+            Assert.AreEqual(Tuple.Create(3, 2), priorityQueue.Dequeue());
+            Assert.AreEqual(Tuple.Create(2, 2), priorityQueue.Dequeue());
+            Assert.AreEqual(Tuple.Create(2, 1), priorityQueue.Dequeue());
+            Assert.AreEqual(Tuple.Create(1, 3), priorityQueue.Dequeue());
+            Assert.AreEqual(Tuple.Create(1, 1), priorityQueue.Dequeue());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void エンキューした数より多くデキューすると例外が発生する()
+        {
+            var priorityQueue = new PriorityQueue<int>();
+            priorityQueue.Enqueue(1);
+            priorityQueue.Enqueue(2);
+
+            priorityQueue.Dequeue();
+            priorityQueue.Dequeue();
+
+            //ここで例外が発生する
+            priorityQueue.Dequeue();
+        }
+    }
+}
